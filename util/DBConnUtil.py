681a98cb63f2fb9ddfbe6f4fea0448854e93aaa8
amisha.india <kumariamisha2022@gmail.com>
2024-05-15 13:50:15 +0530

    connection

--- util/DBConnUtil.py
+++ util/DBConnUtil.py
@@ -1,14 +1,16 @@
-
 import pyodbc
+
 
 class DBConnUtil:
     @staticmethod
     def getDBConn():
         try:
-            connection = pyodbc.connect('Driver={SQL Server};'
-                                        'Server=Amarjeet\SQLEXPRESS;'
-                                        'Database=OrderManagementSystem;'
-                                        'Trusted_Connection=yes;')
+            connection = pyodbc.connect(
+                "Driver={{SQL Server}};"
+                "Server=Amarjeet\SQLEXPRESS;"
+                "Database=OrderManagementSystem;"
+                "Trusted_Connection=yes;"
+            )
             return connection
         except Exception as e:
             print("Error connecting to database:", e)
